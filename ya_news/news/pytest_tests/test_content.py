a2422caa7dc66c50d81dc9da34dc2dc5d3834d1a
Vadim Kuznetsov <darovadraste@gmail.com>
2023-12-25 15:26:37 +0300

    added fixture for detail url with comments, test_content fixes

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -1,40 +1,39 @@
 from django.conf import settings
-from django.urls import reverse
 
-HOME_URL = reverse('news:home')
+from news.forms import CommentForm
 
 
-def test_news_count(client, news_list):
-    response = client.get(HOME_URL)
-    object_list = response.context['object_list']
+def test_news_count(client, news_list, home_url):
+    response = client.get(home_url)
+    object_list = response.context.get('object_list', [])
     news_count = len(object_list)
-    assert news_count <= settings.NEWS_COUNT_ON_HOME_PAGE
+    assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
-def test_news_order(client, news_list):
-    response = client.get(HOME_URL)
-    object_list = response.context['object_list']
+def test_news_order(client, news_list, home_url):
+    response = client.get(home_url)
+    object_list = response.context.get('object_list', [])
     all_dates = [news.date for news in object_list]
     sorted_dates = sorted(all_dates, reverse=True)
     assert sorted_dates == all_dates
 
 
-def test_comments_order(client, comment_list, news_with_comment_id):
-    url = reverse('news:detail', args=news_with_comment_id)
-    response = client.get(url)
+def test_comments_order(client, detail_url_with_comments):
+    response = client.get(detail_url_with_comments)
     assert 'news' in response.context
     news = response.context['news']
-    all_comments = news.comment_set.all()
-    assert all_comments[0].created < all_comments[1].created
+    all_comments = [comment.created for comment in news.comment_set.all()]
+    sorted_comments = sorted(all_comments)
+    assert sorted_comments == all_comments
 
 
-def test_anonymous_client_has_no_form(client, news_with_comment_id):
-    url = reverse('news:detail', args=news_with_comment_id)
-    response = client.get(url)
+def test_anonymous_client_has_no_form(client, detail_url_with_comments):
+    response = client.get(detail_url_with_comments)
     assert 'form' not in response.context
 
 
-def test_authorized_client_has_form(admin_client, news_with_comment_id):
-    url = reverse('news:detail', args=news_with_comment_id)
-    response = admin_client.get(url)
+def test_authorized_client_has_form(admin_client, detail_url_with_comments):
+    response = admin_client.get(detail_url_with_comments)
     assert 'form' in response.context
+    # из-за проверки выше .get() для проверки key-error не требуется, думаю
+    assert isinstance(response.context['form'], CommentForm)
